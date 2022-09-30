a03996dbf5ca0e91337cab923d6a63849af4cc98
CGPxy <52651150+CGPzy@users.noreply.github.com>
2022-09-30 10:34:49 +0800

    Update HAAM.py

--- HAAM.py
+++ HAAM.py
@@ -11,7 +11,7 @@ def expend_as(tensor, rep):
 
 # Channel attentation
 def Channelblock(data, filte):
-	conv1 = Conv2D(filte, (3, 3), padding="same",dilation_rate=(3,3))(data)
+    conv1 = Conv2D(filte, (3, 3), padding="same",dilation_rate=(3,3))(data)
     batch1 = BatchNormalization()(conv1)
     LeakyReLU1 = ReLU()(batch1)
 
@@ -45,8 +45,7 @@ def Channelblock(data, filte):
 
 # spatial attentation
 def Spatialblock(data, channel_data, filte, size):
-
-	conv1 = Conv2D(filte, (3, 3), padding="same")(data)
+    conv1 = Conv2D(filte, (3, 3), padding="same")(data)
     batch1 = BatchNormalization()(conv1)
     LeakyReLU1 = ReLU()(batch1)
 
@@ -63,7 +62,6 @@ def Spatialblock(data, channel_data, filte, size):
     a = expend_as(data3, filte)
     y = multiply([a, channel_data])
 
-
     a1 = 1-data3
     a1 = expend_as(a1, filte)
     y1 = multiply([a1, spatil_data])
@@ -79,6 +77,6 @@ def HAAM(data, filte,size):
 
     channel_data = Channelblock(data=data, filte=filte)
 
-	haam_data = Spatialblock(data, channel_data, filte, size)
+    haam_data = Spatialblock(data, channel_data, filte, size)
 
     return haam_data
